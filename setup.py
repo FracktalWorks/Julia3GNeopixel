b6b9d86625f47ff3cfd5575a8199169d54b05941
Vijay Raghav Varada <vjvarada@fracktal.in>
2017-06-06 14:54:03 +0530

    check for i2S

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ plugin_package = "octoprint_Julia3GNeopixel"
 plugin_name = "Octoprint-Julia3GNeopixel"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
-plugin_version = "0.0.5"
+plugin_version = "0.0.6"
 
 # The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
 # module
@@ -33,7 +33,7 @@ plugin_url = "https://github.com/FracktalWorks/Julia3GNeopixel"
 plugin_license = "AGPLv3"
 
 # Any additional requirements besides OctoPrint should be listed here
-plugin_requires = ['python-smbus']
+plugin_requires = ['smbus-cffi']
 
 ### --------------------------------------------------------------------------------------------------------------------
 ### More advanced options that you usually shouldn't have to touch follow after this point
